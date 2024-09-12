352b91c4886ff2128f287f02b50cb347a79146f9
LancePC <ydingus1@gmail.com>
2024-09-12 00:54:32 -0400

    hw1 Q5 penguins_visualization all subproblem code pending review

--- HW1/HW1_SkeletonCode/penguins_visualization.py
+++ HW1/HW1_SkeletonCode/penguins_visualization.py
@@ -1,7 +1,4 @@
-from cProfile import label
-
-import matplotlib.container
-from matplotlib import matplotlib_fname
+from matplotlib.lines import Line2D
 
 try:
     import numpy as np
@@ -24,17 +21,19 @@ except ImportError:
 def question1():
     # Function to draw histogram
     def draw_species_histogram(data: pd.DataFrame, axis: int):
-        ax = sns.histplot(data=data, x="species", hue="species", ax=axis, discrete=True)
+        ax = sns.histplot(data=data, x="species", hue="species", ax=axis, discrete=True, palette='Set1') # Specify palette
+        ax.legend_.remove() # Remove subplot legend
 
         # Add bar labels
         for c in ax.containers:
             ax.bar_label(c)
 
+    # Load data
     df = palmer.load_csv("data/penguins.csv")
     print(df.head())
 
     # Side by side for easier comparison
-    fig, axs = plt.subplots(1, 2)
+    fig, axs = plt.subplots(1, 2, figsize=(9,6))
 
     # Title and Labels
     fig.suptitle("Palmer Penguins Species Distribution")
@@ -50,8 +49,18 @@ def question1():
     axs[0].set_xlabel("")
     axs[1].set_xlabel("")
 
+    # Manually create legend handles and labels - inspired by stackoverflow
+    # Again - this legend took way too much effort to make (I came back to fix this legend after part 2 and part 3)
+    palette = sns.color_palette('Set1', n_colors=3)
+    labels = ["Adelie", "Gentoo", "Chinstrap"]
+    # Create legend icons with a white line, circular marker, color corresponding to a label
+    handles = [Line2D([0], [0], color='w', marker='o', markerfacecolor=palette[i]) for i in range(3)]
+
+    # Create a single legend for the entire figure
+    fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0.05), ncol=3)
+
     # Cleanup and display
-    plt.tight_layout()
+    plt.tight_layout(rect=(0, 0.05, 1, 1))
     plt.show()
 
 
@@ -65,7 +74,7 @@ def question2():
                                "Body mass (g)"]
 
     # Canvas setup
-    fig, axs = plt.subplots(1, 4)
+    fig, axs = plt.subplots(1, 4, figsize=(9,6))
     fig.suptitle("Numeric Feature Boxplots per Species")
     fig.supxlabel("Species")
 
@@ -87,9 +96,38 @@ def question2():
 
 
 def question3():
-    pass
+    df = palmer.load_csv("data/penguins.csv")
+    numerical_features = ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']
+    numerical_feature_names = ["Bill length (mm)",
+                               "Bill depth (mm)",
+                               "Flipper length (mm)",
+                               "Body mass (g)"]
+    fig, axs = plt.subplots(2, 3, figsize=(9,6))
+    plots = []
+    idx = 0
+
+    for f1 in range(4):
+        for f2 in range(f1+1, 4):
+            cur_ax = axs[idx//3][idx%3]
+            plots.append(sns.scatterplot(data=df, x=numerical_features[f1], y=numerical_features[f2], hue="species",
+                                         ax=cur_ax))
+            cur_ax.set_xlabel(numerical_feature_names[f1])
+            cur_ax.set_ylabel(numerical_feature_names[f2])
+            cur_ax.legend_.remove()
+            idx += 1
+
+    # Steal information from one of the plots and use it create a legend (since the labels are the same across plots)
+    handles, labels = axs[0][0].get_legend_handles_labels()
+    fig.legend(handles=handles, labels=labels, loc='lower center', bbox_to_anchor=(0.5, 0.05), ncol=3)
+
+    fig.suptitle("Numeric Feature Boxplots per Species")
+    plt.tight_layout(rect=(0, 0.1, 1, 1))
+
+    # plt.tight_layout()
+    plt.show()
+
 
 if __name__ == '__main__':
-    # question1()
-    # question2()
+    question1()
+    question2()
     question3()
