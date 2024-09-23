d7bad05c92c75ea66100adfd99416d17b26485f6
LancePC <ydingus1@gmail.com>
2024-09-23 00:45:47 -0400

    finished Q3.1 (d) up to values for table. Nothing has been typed up yet

--- HW2/HW2_SkeletonCode/linear_regression.py
+++ HW2/HW2_SkeletonCode/linear_regression.py
@@ -6,53 +6,57 @@ Follow the instructions in the homework to complete the assignment.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from fontTools.ttLib.tables.E_B_D_T_ import ebdt_bitmap_format_6
+import seaborn as sns
+
 from helper import load_data
 import time
 
 def generate_polynomial_features(X, M):
     """
     Create a polynomial feature mapping from input examples. Each element x
-    in X is mapped to an (M+1)-dimensional polynomial feature vector 
+    in X is mapped to an (M+1)-dimensional polynomial feature vector
     i.e. [1, x, x^2, ...,x^M].
 
     Args:
         X: np.array, shape (N, 1). Each row is one instance.
         M: a non-negative integer
-    
+
     Returns:
         Phi: np.array, shape (N, M+1)
     """
-    # TODO: Implement this function
-    Phi = ???
+    Phi = np.vander(X.T[0], N=M+1, increasing=True)
+
     return Phi
 
 def calculate_squared_loss(X, y, theta):
     """
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         theta: np.array, shape (d,)
-    
+
     Returns:
         loss: float. The empirical risk based on squared loss as defined in the assignment.
     """
-    # TODO: Implement this function
-    loss = ???
+    assert X.shape[0] == y.size and X.shape[1] == theta.size
+    n = X.shape[0]
+    loss = 1/(2 * n) * ((y - X @ theta) @ (y - X @ theta))
     return loss
 
 def calculate_RMS_Error(X, y, theta):
     """
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         theta: np.array, shape (d,)
 
     Returns:
         E_rms: float. The root mean square error as defined in the assignment.
     """
-    # TODO: Implement this function
-    E_rms = ???
-    return E_rms
+    # # TODO: Implement this function
+    # E_rms = ???
+    # return E_rms
 
 
 def ls_gradient_descent(X, y, learning_rate=0):
@@ -61,15 +65,44 @@ def ls_gradient_descent(X, y, learning_rate=0):
     Note:
         - Please use the stopping criteria: number of iterations >= 1e6 or |new_loss - prev_loss| <= 1e-10
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         learning_rate: float, the learning rate for GD
-    
+
     Returns:
         theta: np.array, shape (d,)
     """
-    # TODO: Implement this function
-    theta = ???
+    assert X.shape[0] == y.size
+    n = X.shape[0]
+    d = X.shape[1]
+
+    theta = np.zeros((d,))
+    MAX_ITERS = 1e6
+    k = 0
+    prev_loss = 0
+    cur_loss = 1
+
+    # # Logging
+    # losses = []
+
+    while k < MAX_ITERS and abs(cur_loss - prev_loss) > 1e-10:
+        # Set up next cycle
+        prev_loss = cur_loss
+        cur_loss = calculate_squared_loss(X, y, theta)
+
+        # # Log losses - optional
+        # losses.append(cur_loss)
+
+        for i in range(n):
+            # Update theta
+            theta = theta + learning_rate * (y[i] - theta @ X[i, :]) * X[i, :]
+
+        k += 1
+
+    # # Track losses - optional
+    # sns.lineplot(x=[i for i in range(k)], y=losses)
+    # plt.show()
+    print(f"Iterations:\t{k}")
     return theta
 
 
@@ -79,17 +112,57 @@ def ls_stochastic_gradient_descent(X, y, learning_rate=0):
     Note:
         - Please do not shuffle your data points.
         - Please use the stopping criteria: number of iterations >= 1e6 or |new_loss - prev_loss| <= 1e-10
-    
+
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         learning_rate: float or 'adaptive', the learning rate for SGD
-    
+
     Returns:
         theta: np.array, shape (d,)
     """
-    # TODO: Implement this function
-    theta = ???
+    assert X.shape[0] == y.size
+    n = X.shape[0]
+    d = X.shape[1]
+
+    if str(learning_rate).lower() == 'adaptive':
+        adaptive = True
+    else:
+        adaptive = False
+
+    k = 0
+    theta = np.zeros((d, ))
+    cur_loss = 0
+    prev_loss = 1
+    MAX_ITERS = 1e6
+
+    # Logging
+    # losses = []
+
+    while k < MAX_ITERS and abs(cur_loss - prev_loss) > 1e-10:
+        prev_loss = cur_loss
+        cur_loss = calculate_squared_loss(X, y, theta)
+
+        # Dynamic lr
+        if adaptive:
+            learning_rate = 1/(1+k)
+
+        # # Logging
+        # losses.append(cur_loss)
+
+        # Use a permutation of length n to determine the order we process the data
+        perm = np.random.permutation(n)
+        for i in perm:
+            theta = theta + learning_rate * (y[i] - theta @ X[i, :]) * X[i, :]
+
+        k += 1
+
+    # # Logging
+    # sns.lineplot(x=[i for i in range(k)], y=losses)
+    # plt.show()
+
+    print(f"Iterations:\t{k}")
+
     return theta
 
 
@@ -98,15 +171,17 @@ def ls_closed_form_solution(X, y, reg_param=0):
     Implements the closed form solution for least squares regression.
 
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         reg_param: float, an optional regularization parameter
 
     Returns:
         theta: np.array, shape (d,)
     """
-    # TODO: Implement this function
-    theta = ???
+    assert X.shape[0] == y.size
+
+    # theta = np.linalg.inv(X.T @ X) @ X.T @ y
+    theta = np.linalg.pinv(X) @ y
     return theta
 
 
@@ -115,7 +190,7 @@ def weighted_ls_closed_form_solution(X, y, weights, reg_param=0):
     Implements the closed form solution for weighted least squares regression.
 
     Args:
-        X: np.array, shape (N, d) 
+        X: np.array, shape (N, d)
         y: np.array, shape (N,)
         weights: np.array, shape (N,), the weights for each data point
         reg_param: float, an optional regularization parameter
@@ -123,9 +198,9 @@ def weighted_ls_closed_form_solution(X, y, weights, reg_param=0):
     Returns:
         theta: np.array, shape (d,)
     """
-    # TODO: Implement this function
-    theta = ???
-    return theta
+    # # TODO: Implement this function
+    # theta = ???
+    # return theta
 
 
 def part_1(fname_train):
@@ -137,13 +212,42 @@ def part_1(fname_train):
     X_train, y_train = load_data(fname_train)
     Phi_train = generate_polynomial_features(X_train, 1)
 
-    # Example of how to use the functions
-    start = time.process_time()
-    theta = ls_stochastic_gradient_descent(Phi_train, y_train, learning_rate=0.01)
-    print('Time elapsed:', time.process_time() - start)
+    # # Example of how to use the functions
+    # start = time.process_time()
+    # theta = ls_stochastic_gradient_descent(Phi_train, y_train, learning_rate=0.01)
+    # # theta = ls_gradient_descent(Phi_train, y_train, learning_rate=0.01)
+
+    # print('Time elapsed:', time.process_time() - start)
 
     # TODO: Add more code here to complete part 1
     ##############################
+
+    lrs = [1e-4, 1e-3, 1e-2, 1e-1]
+
+    # GD
+    print("GD\n")
+    for lr in lrs:
+        print("=="*4 + f"learning rate:\t{lr}" + "=="*4)
+        start = time.process_time()
+        theta = ls_gradient_descent(Phi_train, y_train, learning_rate=lr)
+        print('Time elapsed:', time.process_time() - start)
+        print(f"Theta:\t{theta}\n")
+
+    # SGD
+    print("SGD\n")
+    for lr in lrs:
+        print("=="*4 + f"learning rate:\t{lr}" + "=="*4)
+        start = time.process_time()
+        theta = ls_stochastic_gradient_descent(Phi_train, y_train, learning_rate=lr)
+        print('Time elapsed:', time.process_time() - start)
+        print(f"Theta:\t{theta}\n")
+
+    # Closed form
+    print("Closed form\n")
+    start = time.process_time()
+    theta = ls_closed_form_solution(Phi_train, y_train)
+    print('Time elapsed:', time.process_time() - start)
+    print(f"Theta:\t{theta}\n")
 
     print("Done!")
 
@@ -180,9 +284,9 @@ def part_3(fname_train, fname_validation):
 
 def main(fname_train, fname_validation):
     part_1(fname_train)
-    part_2(fname_train, fname_validation)
-    part_3(fname_train, fname_validation)
+    # part_2(fname_train, fname_validation)
+    # part_3(fname_train, fname_validation)
 
 
 if __name__ == '__main__':
-    main("dataset/linreg_train.csv", "dataset/linreg_validation.csv")
+    main("data/linreg_train.csv", "data/linreg_validation.csv")
